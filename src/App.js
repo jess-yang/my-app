c5210c76cd33bb4c8dcabf3ccbf0b24e5d1c766e
Jessica Yang <jessica.yang@berkeley.edu>
2020-09-25 04:51:27 -0700

    take 2

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          What up, I'm Jessica 
+        </h1>
+        <p>
+          This website is currently under construction.
+        </p>
+        <p>
+          Check back soon!
+        </p>
+
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
