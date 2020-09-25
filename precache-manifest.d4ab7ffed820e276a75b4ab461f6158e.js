d39698189df076109f2a3b1de4a008fb8dc7edd3
Jessica Yang <jessica.yang@berkeley.edu>
2020-09-25 05:04:33 -0700

    Updates

---
+++ precache-manifest.d4ab7ffed820e276a75b4ab461f6158e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bdbae8e334e9f1e26f023c462640ca9",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "27a22f67b65f7a15dfbe",
+    "url": "/my-app/static/css/main.e2453600.chunk.css"
+  },
+  {
+    "revision": "67a6b61e3c815ed25f6c",
+    "url": "/my-app/static/js/2.180d86fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.180d86fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27a22f67b65f7a15dfbe",
+    "url": "/my-app/static/js/main.7c8d1146.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
